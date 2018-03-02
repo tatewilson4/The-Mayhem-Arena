60b9675a3feb4f115be7ce7a885d0a62e47f251c
Tate Wilson <tatewilson4@gmail.com>
2018-03-02 14:55:44 -0700

    created opening page, linked files, and start button

---
+++ project1/js/app.js
@@ -0,0 +1,7 @@
+//Create a clickable start button
+$(()=>{
+  let $divStart = $('<div><button class="button">START</button></li>');
+  $divStart.addClass('start');
+  $('h1').append($divStart);
+
+})
