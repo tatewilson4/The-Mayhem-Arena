99184a81b76dadc1fee008bdf11d3599fc953659
Tate Wilson <tatewilson4@gmail.com>
2018-03-03 10:18:35 -0700

    still trying to push the project

--- project1/js/app.js
+++ project1/js/app.js
@@ -1,7 +1,81 @@
-//Create a clickable start button
+
 $(()=>{
-  let $divStart = $('<div><button class="button">START</button></li>');
+  //creat stats for fighter 1 and cpu
+  class $Fighter{
+    constructor(hull, attack, defend){
+      this.hull = 100;
+      this.attack = 50;
+      this.defend = 50;
+    }
+  }
+  //creat attack
+  class $Cpu {
+    constructor(hull, attack, defend){
+      this.hull = 100;
+      this.attack = 50;
+      this.defend = 50;
+    }
+  }
+
+  // if attack happens cpu health goes down
+  $attack = () => {
+    $cpu.hull = $cpu.hull - $player.attack;
+  }
+
+  $cpuAttack = () => {
+    (player.hull - cpu.attack);
+  }
+
+
+
+  //Create a clickable start button
+  let $divStart = $('<div><button class="button">START</button></div>');
+  //create fighter 1 and 2
+  let $fighter = $('<div><button class="button2">CHOOSE</button></div>');
+  // let $fighter2 = $('<div><button class="button3">CHOOSE</button></div>');
+  //create fighter 1 and 2 fight board
+  let $battleScreen = $('<div><button class="button3">ATTACK</button><button class="button4">DEFEND</button></div>');
+  let $battleScreen2 = $('<div><button class="button5">ATTACK</button><button class="button6">DEFEND</button></div>');
   $divStart.addClass('start');
   $('h1').append($divStart);
 
-})
+//remove click button
+//add choose fighter buttons
+  $('.button').on('click' , (event) => {
+    $(event.target).parent().remove();
+    $fighter.addClass('fighter');
+    $('h2').append($fighter);
+    // $fighter2.addClass('fighter2')
+    // $('h2').append($fighter2);
+    $('.button2').on('click' , (event) => {
+      $(event.currentTarget).parent().remove();
+      $battleScreen.addClass('battleScreen');
+      $battleScreen2.addClass('battleScreen2');
+      // $battleScreen2.addClass('battleScreen2');
+      $('h3').append($battleScreen);
+      $('h3').append($battleScreen2);
+      // $('h2').append($battleScreen2);
+      $('.button3').on('click' , (event) => {
+        $attack();
+        console.log($cpu);
+      })
+      $('.button4').on('click' , (event) => {
+        console.log('1');
+      })
+      $('.button5').on('click' , (event) => {
+        console.log('1');
+      })
+      $('.button6').on('click' , (event) => {
+        console.log('1');
+      })
+    });
+  });
+
+
+
+
+//creat a variable for cpu and fighter
+
+const $cpu = new $Cpu('Bad');
+const $player = new $Fighter('player');
+});
