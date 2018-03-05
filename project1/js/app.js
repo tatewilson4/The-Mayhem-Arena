994e280a9009ec272988fdf97085b80050f9a2f2
Tate Wilson <tatewilson4@gmail.com>
2018-03-05 12:36:46 -0700

    adding round 2, button, battle screen, retrurn players health to 100

--- project1/js/app.js
+++ project1/js/app.js
@@ -36,8 +36,11 @@ $(()=>{
   //create fighter 1 and 2 fight board
   let $battleScreen = $('<div><button class="button3">ATTACK</button><button class="button4">DEFEND</button></div>');
   let $battleScreen2 = $('<div><button class="button5">ATTACK</button><button class="button6">DEFEND</button></div>');
+  // let $battleScreen3 = $('<div><button class="button7">ATTACK</button><button class="button8">DEFEND</button></div>');
+  // let $battleScreen4 = $('<div><button class="button9">ATTACK</button><button class="button10">DEFEND</button></div>');
   $divStart.addClass('start');
   $('h1').append($divStart);
+
 
 //remove click button
 //add choose fighter buttons
@@ -52,29 +55,53 @@ $(()=>{
       $battleScreen.addClass('battleScreen');
       $battleScreen2.addClass('battleScreen2');
       // $battleScreen2.addClass('battleScreen2');
+      //added alert for round 1
+      alert('This is round 1');
       $('h3').append($battleScreen);
       $('h3').append($battleScreen2);
       // $('h2').append($battleScreen2);
       //attack and defend buttons for cpu and player1
       $('.button3').on('click' , (event) => {
         $attack();
-        console.log($cpu);
+        // console.log($cpu);
+        if($cpu.hull <= 0){
+          alert('You killed cpu, do you want to continue to the next round?' , 'Yes/No');
+          $round2();
+        }
       })
       $('.button4').on('click' , (event) => {
-        console.log('1');
+        // console.log('1');
       })
       $('.button5').on('click' , (event) => {
         $cpuAttack();
-        console.log($player);
+        // if($player.hull <= 0){
+        //   alert('You were killed, do you want to continue to the next round?');
+        //   $round2();
+        // }
       })
       $('.button6').on('click' , (event) => {
         console.log('1');
       })
     });
+
+    //Round 2
+    $round2 = () => {
+      $player.hull = 100;
+      $cpu.hull = 100;
+      $battleScreen.remove();
+      $battleScreen2.remove();
+      alert('This is round 2');
+      $battleScreen.addClass('battleScreen');
+      $battleScreen2.addClass('battleScreen2');
+      $('h3').append($battleScreen);
+      $('h3').append($battleScreen2);
+      $('.button3').on('click' , (event) => {
+        $attack();
+        console.log($cpu);
+      })
+    }
+
   });
-
-
-
 
 //creat a variable for cpu and fighter
 
